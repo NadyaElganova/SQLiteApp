c3a20613e74f64fd04c14f1809402ae9e082937a
NadyaElganova <122999097+NadyaElganova@users.noreply.github.com>
2024-08-09 21:24:12 +0400

    init

---
+++ app/src/main/java/com/first/sqliteapp/UsersAdapter.java
@@ -0,0 +1,50 @@
+package com.first.sqliteapp;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.List;
+
+public class UsersAdapter extends RecyclerView.Adapter<UsersAdapter.UserViewHolder> {
+
+    private List<User> users;
+
+    public UsersAdapter(List<User> users) {
+        this.users = users;
+    }
+
+    @NonNull
+    @Override
+    public UserViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext()).inflate(android.R.layout.simple_list_item_1, parent, false);
+        return new UserViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull UserViewHolder holder, int position) {
+        User user = users.get(position);
+        holder.nameTextView.setText(user.getName());
+        holder.emailTextView.setText(user.getEmail());
+    }
+
+    @Override
+    public int getItemCount() {
+        return users.size();
+    }
+
+    static class UserViewHolder extends RecyclerView.ViewHolder {
+        TextView nameTextView;
+        TextView emailTextView;
+
+        public UserViewHolder(@NonNull View itemView) {
+            super(itemView);
+            nameTextView = itemView.findViewById(android.R.id.text1);
+            emailTextView = itemView.findViewById(android.R.id.text2);
+        }
+    }
+}
